312c42ef9d45073cf745609a96f2138831619d17
bysu <dobby.indev@gmail.com>
2025-12-17 20:16:12 +0900

    feat: set default application timezone to UTC during initialization

--- src/main/kotlin/com/rootbly/openpulse/OpenpulseApplication.kt
+++ src/main/kotlin/com/rootbly/openpulse/OpenpulseApplication.kt
@@ -1,13 +1,21 @@
 package com.rootbly.openpulse
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.TimeZone
 
 
 @EnableScheduling
 @SpringBootApplication
-class OpenpulseApplication
+class OpenpulseApplication {
+
+    @PostConstruct
+    fun init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<OpenpulseApplication>(*args)
